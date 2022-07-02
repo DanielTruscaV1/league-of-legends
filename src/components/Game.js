dabd76278539078350d456aaf2035499543a17bc
DanielTruscaV1 <danieltrusca2016@gmail.com>
2022-07-02 12:35:42 +0300

    Updated the "The game" page of the website.

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,6 +4,9 @@ import image7 from "./images/image7.webp";
 import image8 from "./images/image8.jpg";
 import image9 from "./images/image9.webp";
 import image10 from "./images/image10.jfif";
+import image11 from "./images/image11.webp";
+import image12 from "./images/image12.jpg";
+import image13 from "./images/image13.PNG";
 
 function Game() {
   return (
@@ -35,6 +38,32 @@ function Game() {
       <p>Baron Nashor is the most powerful monster in the jungle. If a team manages to kill the Baron, it receives a bonus to attack damage and strength of abilities, improved 'Recall', and the power of minions around its members increases significantly.</p>
       <h3>DRAGONS</h3>
       <p>Dragons are powerful monsters, which offer unique bonuses depending on the element of the slain dragon. In the game there are five elemental dragons and an ancient Dragon.</p>
+      <h1>CHOOSE YOUR COLOR</h1>
+      <img src={image11}/>
+      <p>The recommended team composition consists of five positions. Each aisle fits best with certain types of champions and roles - you can try to play all the aisles in a row or you can focus on the one you like best.</p>
+      <h3>TOP LANE</h3>
+      <p>The champions on the top aisle are the lone and resilient fighters of your team. Their goal is to protect their lane and focus their attacks on the strongest members of the opposing team.</p>
+      <h3>JUNGLE</h3>
+      <p>Junglers live to hunt down their enemies. I walk skilfully and cunningly from one corridor to another and keep an eye on the most important neutral monsters, attacking exactly when the opponents let their guard down.</p>
+      <h3>MID LANE</h3>
+      <p>The middle lane champions quickly cause enormous damage, both alone and with the team. For them, the fight is like a dangerous dance, in which they always try to find opportunities to catch their enemies on the wrong foot.</p>
+      <h3>BOTTOM LANE</h3>
+      <p>Downhill champions are the explosive force of your team. They are a precious resource, so you must be protected at the beginning of the game, until you gather enough gold and enough experience to be able to win the match for your team.</p>
+      <h3>SUPPORT CHAMPION</h3>
+      <p>Supporting champions are the team's goalkeepers. They keep their teammates alive and help them kill their opponents; In addition, it protects the colleague on the lower aisle until he becomes stronger.</p>
+      <h1>INCREASE THE POWER OF YOUR CHAMPION</h1>
+      <img src={image12}/>
+      <p>Champions become stronger as they gain experience to move to the next level, as well as gold to buy stronger items. These two factors are crucial to dominate the enemy team and destroy its base.</p>
+      <p>When champions gain a certain amount of experience, they automatically move to the next level and can unlock or improve certain skills; in addition, their basic attributes become stronger. You can gain experience by killing enemy units and champions, participating in an assassination, and destroying your opponent's defensive structures.</p>
+      <p>Gold is the currency in the game through which you can buy items for your champion. You can collect gold by killing enemy units and champions, by participating in an assassination, by destroying your opponent's defensive structures, and when you equip items that offer gold.</p>
+      <p>With the help of the store, you can buy items with the gold earned or sell them. The store can only be accessed from the fountain.</p>
+      <h1>UNLOCK YOUR SKILLS</h1>
+      <img src={image13}/>
+      <p>Champions have five main abilities, two special spells and can keep up to seven items in inventory at the same time. In order for your team to win, you need to find out the optimal order for skills, what summoning spells to use and what items to buy.</p>
+      <p>Most champions have a unique set of five abilities: one passive, three basic, and one supreme. These abilities can be used with the Q, W, E and R keys.</p>
+      <p>Summoner spells are abilities with unique effects, which can be used by champions with the D and F keys. There are many spells of this type in the game, but the most commonly used are 'Flash', 'Teleport', 'Smite' and ' Ignition '.</p>
+      <p>Objects improve the performance of the chosen champion: they increase his movement speed and damage and reduce his reactivation times of abilities. Unlike talismans, items cost gold to be purchased.</p>
+
     </div>
   )
 }
